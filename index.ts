d662f2af33e81197b81280fa96170ea31c106063
Łukasz Miłkowski <lukanus@uaznia.net>
2021-10-19 16:39:18 +0200

    few changes

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as graphql from "graphql";
 import * as yargs from "yargs";
 
 import { toSchemaObjects } from "./convert";
-import { readFileSync, existsSync, writeFileSync } from "fs";
+import { readFileSync, existsSync, writeFileSync, mkdirSync } from "fs";
 import { readdir } from "fs/promises";
 
 function main() {
@@ -16,7 +16,7 @@ async function process() {
       .option("dir", {
         description: "the directory to scan for input files",
         alias: "d",
-        type: "number",
+        type: "string",
       })
       .default("dir", ".", "read from the same folder")
       .option("output", {
@@ -40,6 +40,7 @@ async function process() {
 
     await walkPath(
       argv.dir.toString(),
+      argv.dir.toString(),
       argv.pattern?.toString(),
       argv.output.toString()
     );
@@ -48,7 +49,7 @@ async function process() {
   }
 }
 
-async function walkPath(path: string, pattern: string, output: string) {
+async function walkPath(basepath: string,  path: string, pattern: string, output: string) {
   try {
     const files = await readdir(path, { withFileTypes: true });
     for (const file of files) {
@@ -56,14 +57,19 @@ async function walkPath(path: string, pattern: string, output: string) {
         continue;
       }
 
+      //console.log(file, path, pattern, output);
       if (file.isDirectory()) {
-        await walkPath(path, pattern, output);
+        await walkPath(basepath, path+"/"+file.name, pattern, output);
       } else {
         if (file.name.includes(pattern)) {
-          const fContents = readFileSync(path + file.name, "utf-8");
+          const fContents = readFileSync(path + "/" + file.name, "utf-8");
           const protoDocument = t.parse(fContents.toString()) as t.ProtoDocument;
           const gqlp = graphql.print(toSchemaObjects(protoDocument));
-          const filename = nextFilename(file.name, output, 0);
+
+          const dir = output + path.replace(basepath, "");
+          mkdirSync(dir, { recursive: true });
+          const filename = nextFilename(file.name, dir, 0);
+
           writeFileSync(filename, gqlp);
         }
       }
@@ -74,6 +80,7 @@ async function walkPath(path: string, pattern: string, output: string) {
 }
 
 function nextFilename(name: string, dir: string, iter: number): string {
+  console.log(name, dir, iter );
   if (iter == 0 && !existsSync(dir + "/" + name + ".graphql")) {
     return dir + "/" + name + ".graphql";
   }
